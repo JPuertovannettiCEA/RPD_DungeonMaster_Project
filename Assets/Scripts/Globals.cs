e6590640dd84fb0cee90008552bd5632322de852
arvind <gd04arvind@thecea.ca>
2021-10-14 00:33:49 -0700

    UI Pause menu

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public static class Globals
 {
-    public static int DeadAI;
-    public static int AICount = 6;
+    public static float DeadAI;
+    public static float AICount = 6;
     public static int enemySpawnedMax = 0;
     public static float PlayerHealth = 100f;
     public static float AIAttackValue = 5f;
@@ -14,5 +12,7 @@ public static class Globals
     public static float EnemyAIHealth = 100f;
     public static bool hittingEnemy = false;
     public static Slider _healthBar;
+    public static Slider _chestBar;
+    public static TMP_Text _infoText;
 
 }
